149318e21f1c7b7b3f6beb840e88843f00b15db0
ikeraguero <ikerpires407@gmail.com>
2025-12-24 19:45:17 -0300

    Add Remaining HeroSection unit tests

--- src/testing/HeroSection.test.tsx
+++ src/testing/HeroSection.test.tsx
@@ -1,16 +1,33 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import { HeroSection } from '@/components/HeroSection';
 
 describe('HeroSection', () => {
-  it('renders the hero image', () => {
-    render(<HeroSection />);
 
+  beforeAll(() => {
+    render(<HeroSection />);
+  })
+  it('should render the hero image', () => {
     const image = screen.getByRole('img', {
       name: /hero section image/i,
     });
 
     expect(image).toBeInTheDocument();
   });
+
+  it('should render the hero button', () => {
+    const button = screen.getByRole('button', {
+      name: /get started/i
+    })
+
+    expect(button).toBeInTheDocument();
+  })
+
+  it('should render the hero text', () => {
+    expect(screen.getByText(/track races you've watched/i)).toBeInTheDocument();
+    expect(screen.getByText(/unlock achievements/i)).toBeInTheDocument();
+    expect(screen.getByText(/tell your friends what's good/i)).toBeInTheDocument();
+  })
+
 });
