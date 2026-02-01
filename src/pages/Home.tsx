74f6bdfbf4de43f0c59c6928b5f7fc6a37ae3cec
ikeraguero <ikerpires407@gmail.com>
2026-01-31 22:43:29 -0300

    Add Login Modal State Management

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import { HeroSection } from "@/components/HeroSection";
+import { LoginModal } from "@/components/LoginModal";
+import { useState } from "react";
 
 export default function Home() {
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+    function closeLoginModal() {
+        setIsLoginModalOpen(false);
+    }
+
+    function openLoginModal() {
+        setIsLoginModalOpen(true)
+    }
+
     return <>
-        <HeroSection />
+        <HeroSection onOpen={openLoginModal}/>
+        <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal}/>
     </>
 }
